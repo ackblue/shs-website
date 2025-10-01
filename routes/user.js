9aeba7bc9644fce18185cd637b78d20f57a7e921
merttdemirr <demir_14@windowslive.com>
2025-10-01 16:30:32 +0300

    Ana sayfa ve about sayfası eklendi

--- routes/user.js
+++ routes/user.js
@@ -7,5 +7,9 @@ router.use("/about",function(req,res){
     res.sendFile(path.join(__dirname,"../views/users","about2.html"))
 })
 
+router.use("/üye",function(req,res){
+    res.sendFile(path.join(__dirname,"../views/users","want_member.html"))
+})
+
 module.exports=router;
 
